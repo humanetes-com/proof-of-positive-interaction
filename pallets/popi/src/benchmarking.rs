7ac74778e4586a319200be2620bcad0ed8bdad1b
Andrew Cooksey <andrewcooksey42@gmail.com>
2023-12-05 19:48:46 -0600

    Removing benchmarking for now. It's causing issues that can be worked on later.

--- pallets/popi/src/benchmarking.rs
+++ pallets/popi/src/benchmarking.rs
@@ -1,35 +1,35 @@
 //! Benchmarking setup for pallet-popi
-#![cfg(feature = "runtime-benchmarks")]
-use super::*;
+// #![cfg(feature = "runtime-benchmarks")]
+// use super::*;
 
-#[allow(unused)]
-use crate::Pallet as Popi;
-use frame_benchmarking::v2::*;
-use frame_system::RawOrigin;
+// #[allow(unused)]
+// use crate::Pallet as Popi;
+// use frame_benchmarking::v2::*;
+// use frame_system::RawOrigin;
 
-#[benchmarks]
-mod benchmarks {
-	use super::*;
+// #[benchmarks]
+// mod benchmarks {
+// 	use super::*;
 
-	#[benchmark]
-	fn do_something() {
-		let value = 100u32.into();
-		let caller: T::AccountId = whitelisted_caller();
-		#[extrinsic_call]
-		do_something(RawOrigin::Signed(caller), value);
+// 	#[benchmark]
+// 	fn do_something() {
+// 		let value = 100u32.into();
+// 		let caller: T::AccountId = whitelisted_caller();
+// 		#[extrinsic_call]
+// 		do_something(RawOrigin::Signed(caller), value);
 
-		assert_eq!(Something::<T>::get(), Some(value));
-	}
+// 		assert_eq!(Something::<T>::get(), Some(value));
+// 	}
 
-	#[benchmark]
-	fn cause_error() {
-		Something::<T>::put(100u32);
-		let caller: T::AccountId = whitelisted_caller();
-		#[extrinsic_call]
-		cause_error(RawOrigin::Signed(caller));
+// 	#[benchmark]
+// 	fn cause_error() {
+// 		Something::<T>::put(100u32);
+// 		let caller: T::AccountId = whitelisted_caller();
+// 		#[extrinsic_call]
+// 		cause_error(RawOrigin::Signed(caller));
 
-		assert_eq!(Something::<T>::get(), Some(101u32));
-	}
+// 		assert_eq!(Something::<T>::get(), Some(101u32));
+// 	}
 
-	impl_benchmark_test_suite!(Popi, crate::mock::new_test_ext(), crate::mock::Test);
-}
+// 	impl_benchmark_test_suite!(Popi, crate::mock::new_test_ext(), crate::mock::Test);
+// }
